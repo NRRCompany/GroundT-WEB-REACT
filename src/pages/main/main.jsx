36b15ea4b11c5ac22cb59e8657a52f1a654fea58
soljjang777 <vivace8520vi@naver.com>
2023-12-16 13:08:55 +0900

    6 commit

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 
 const Main = () => {
+    const homeDivWrapStyleObj = {
+        display: 'flex',
+        flexWrap: 'wrap',
+    }
+
+    const homDivStyleObj = {
+        boxSizing: 'border-box',
+        width: '50%',
+        border:'1px solid black',
+        padding: '10px',
+    }
+
     return (
-        <div>메인</div>
+        <div className="homeDivWrap" style={homeDivWrapStyleObj}>
+            <div className="homeDiv" style={homDivStyleObj}>
+                <div>사용자 프로필</div>
+                <div>
+                    <ul>
+                        <li>직원명 :</li>
+                        <li>소속 :</li>
+                        <li>직책 :</li>
+                    </ul>
+                </div>
+            </div>
+            <div  className="homeDiv" style={homDivStyleObj}>
+                <div>근무 시간</div>
+                <div>그래프가 들어갑니다</div>
+            </div>
+            <div  className="homeDiv" style={homDivStyleObj}>
+                <div>근태 정보</div>
+                <div>11/07 월요일
+                    <div>출근 버튼</div>
+                    <div>퇴근 버튼</div>
+                </div>
+                <div>12 : 00 : 00</div>
+            </div>
+        </div>
     );
 }
-export default Main;
+export default Main
