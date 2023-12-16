63781e4f1f841ab6c1c3e0bb7f62ca64538fc2d0
soljjang777 <vivace8520vi@naver.com>
2023-12-16 12:45:17 +0900

    5 commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const Navigation = () => {
-  return (
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-              <Link to="/">
-                  <div className="nav_logo">
-                      Ground <span>T</span>
-                  </div>
-              </Link>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                      data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                      aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul className="navbar-nav">
-                      <li className="nav-item">
-                          <Link to="/menu1">
-                              <a className="nav-link active" aria-current="page" href="#">menu1</a>
-                          </Link>
-                      </li>
-                      <li className="nav-item dropdown">
-                          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                             aria-expanded="false">
-                              Dropdown link
-                          </a>
-                          <ul className="dropdown-menu">
-                              <li><a className="dropdown-item" href="#">Action</a></li>
-                              <li><a className="dropdown-item" href="#">Another action</a></li>
-                              <li><a className="dropdown-item" href="#">Something else here</a></li>
-                          </ul>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
-  );
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Link to="/">
+                    <div className="nav_logo">
+                        Ground <span>T</span>
+                    </div>
+                </Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to="/근태관리">
+                            <Nav.Link>근태관리</Nav.Link>
+                        </Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Navigation;
