a26fba70aab81ce6da12c5c782eae89160e3fca7
SerimAhn <dkstpfla1234@gmail.com>
2024-01-03 22:24:55 +0900

    feat: [직원 관리] [직원 추가] 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import {Route, Routes} from 'react-router-dom';
+import React from "react";
 import Home from "./pages/main/main.jsx"
 import Login from "./pages/login/login.jsx"
 import FindPassword from "./pages/login/findPassword";
 import SettingPassword from "./pages/login/settingPassword";
+import EmployeeManagement from "./pages/admin/employeeManagement";
+import EmployeeAdd from "./pages/admin/employeeAdd";
 
 
 function App() {
@@ -14,6 +17,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/findPassword" element={<FindPassword />} />
             <Route path="/settingPassword" element={<SettingPassword />} />
+            <Route path="/employeeManagement" element={<EmployeeManagement />} />
+            <Route path="/employeeAdd" element={<EmployeeAdd />} />
         </Routes>
     </div>
   );
